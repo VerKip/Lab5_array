67842eb78b5ab6147494d23de33841020c6ec8fc
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-07-31 01:09:34 +0300

    Add project files.

---
+++ Homework_array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = new int[7];
+            int S=0;
+            int k = 0;
+            Console.WriteLine("Введите последовательно семь чисел");
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                S += array[i];
+                k++;
+            }
+            Console.WriteLine("Среднее арифметическое равно ");
+            Console.WriteLine((double)S/k);
+            Console.ReadKey();
+        }
+    }
+}
